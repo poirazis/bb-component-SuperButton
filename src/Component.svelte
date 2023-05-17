bed6c809144f4de6efe789eb74775211d33956a8
poirazis <poirazis@gmail.com>
2023-05-17 20:00:20 +0300

    Version 1.0.0

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,32 @@
 <script>
   import { getContext } from "svelte"
 
+
+  export let size
+  export let icon
   export let text
+  export let emphasized
+  export let quiet
+  export let selected
+  export let disabled
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
+
 </script>
 
-<div use:styleable={$component.styles}>
-  This is a custom component. The text setting is: {text}.
-</div>
+<button use:styleable={$component.styles} 
+  class:is-selected={selected}    
+  class:spectrum-ActionButton--emphasized={emphasized}
+  class:spectrum-ActionButton--quiet={quiet}
+  class="spectrum-ActionButton spectrum-ActionButton--size{size}"
+  {disabled} >
+  {#if icon && text != "" }
+    <i style:padding-right={"0.5rem"} class="{icon} {size}"/>
+    <span class="spectrum-ActionButton-label">{text}</span>
+  {:else if icon }
+    <i class="{icon} {size}"/>
+  {:else}
+   <span class="spectrum-ActionButton-label">{text}</span>
+  {/if}
+</button>
