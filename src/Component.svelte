3291e9efa2c46e5f58c1b529192ba5857e87adf6
poirazis <poirazis@gmail.com>
2023-05-18 15:10:00 +0300

    1.0.1 - minor fixes

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,6 +1,9 @@
 <script>
   import { getContext } from "svelte"
 
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+  const buttonGroupStore = getContext("super-action-button-group")
 
   export let size
   export let icon
@@ -9,22 +12,24 @@
   export let quiet
   export let selected
   export let disabled
+  export let onClick
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
-
+  function handleClick() {
+    console.log("Button Clicked")
+    if (onClick) onClick();
+  }
 </script>
 
-<button use:styleable={$component.styles} 
+<button on:click={handleClick} use:styleable={$component.styles} 
   class:is-selected={selected}    
   class:spectrum-ActionButton--emphasized={emphasized}
-  class:spectrum-ActionButton--quiet={quiet}
-  class="spectrum-ActionButton spectrum-ActionButton--size{size}"
+  class:spectrum-ActionButton--quiet={$buttonGroupStore?.quiet || quiet }
+  class="spectrum-ActionButton spectrum-ActionButton--size{$buttonGroupStore?.size || size}"
   {disabled} >
-  {#if icon && text != "" }
-    <i style:padding-right={"0.5rem"} class="{icon} {size}"/>
+  {#if icon && text != "" && !$buttonGroupStore?.iconOnly}
+    <i style:padding-right={"0.5rem"} class="{icon}"/>
     <span class="spectrum-ActionButton-label">{text}</span>
-  {:else if icon }
+  {:else if icon && $buttonGroupStore?.iconOnly }
     <i class="{icon} {size}"/>
   {:else}
    <span class="spectrum-ActionButton-label">{text}</span>
