415effcd5ff59fb97062683bd7ab6b6680f4f451
poirazis <poirazis@gmail.com>
2024-03-03 17:56:46 +0200

    1.2.0 - Action Loops

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,3 +1,4 @@
+
 <script>
   import { getContext } from "svelte"
 
@@ -6,7 +7,9 @@
   const buttonGroupStore = getContext("super-action-button-group")
 
   export let size
+
   export let icon
+  export let iconOnly
   export let text
   export let emphasized
   export let quiet
@@ -14,23 +17,64 @@
   export let disabled
   export let onClick
 
-  function handleClick() {
-    if (onClick) onClick();
+  export let loopActions
+  export let loopSource
+  export let loopDelay
+  export let loopEvent
+  export let onLoopStart
+  export let onLoopEnd
+
+  async function handleClick() {
+    if ( loopActions ) {
+      disabled = true;
+      if ( Array.isArray(loopSource) && loopEvent ) {
+        if ( onLoopStart ) onLoopStart( { iterations : loopSource.length } )
+        for (var i = 0; i < loopSource.length; i++) {
+          loopEvent({idx : i, value: loopSource[i]})
+          await sleep(loopDelay);
+         }
+      }
+      if ( onLoopEnd ) onLoopEnd();
+      disabled = false;
+    } else if (onClick) {
+      disabled = true;
+      onClick();
+      disabled = false;
+    }
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 </script>
 
-<button on:click={handleClick} use:styleable={$component.styles} 
+<button on:click|stopPropagation={handleClick} use:styleable={$component.styles} 
   class:is-selected={selected}    
   class:spectrum-ActionButton--emphasized={emphasized}
   class:spectrum-ActionButton--quiet={$buttonGroupStore?.quiet || quiet }
   class="spectrum-ActionButton spectrum-ActionButton--size{$buttonGroupStore?.size || size}"
   {disabled} >
-  {#if icon && text != "" && !$buttonGroupStore?.iconOnly}
-    <i style:padding-right={"0.5rem"} class="{icon}"/>
+  {#if icon && !$buttonGroupStore?.iconOnly && !iconOnly}
+    <i style:padding-right={ text != "" ? "0.5rem" : "0rem"} class="{icon}"/>
     <span class="spectrum-ActionButton-label">{text}</span>
-  {:else if icon && $buttonGroupStore?.iconOnly }
+  {:else if icon && ($buttonGroupStore?.iconOnly || iconOnly )}
     <i class="{icon} {size}"/>
   {:else}
    <span class="spectrum-ActionButton-label">{text}</span>
   {/if}
 </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
